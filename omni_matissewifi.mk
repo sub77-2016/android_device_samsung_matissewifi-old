79aa56ae302a2bb0ee94c4e011fceeb988249910
sub77 <sub77@ymail.com>
2015-02-10 13:12:41 +0100

    omnified!

--- omni_matissewifi.mk
+++ omni_matissewifi.mk
@@ -1,23 +1,30 @@
+#
 # Copyright (C) 2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/samsung/matissewifi/full_matissewifi.mk)
+$(call inherit-product, vendor/omni/config/common_tablet.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := SM-T530
-PRODUCT_NAME := cm_matissewifi
+# Inherit from matissewifi device
+$(call inherit-product, device/samsung/matissewifi/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := matissewifi
+PRODUCT_NAME := full_matissewifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-T530
+PRODUCT_MANUFACTURER := samsung
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo cm_matissewifi-userdebug
+add_lunch_combo omni_matissewifi-userdebug
 
