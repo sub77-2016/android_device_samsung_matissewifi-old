435c6f51bf4b3d74b93b401aba5bf3442840f4b4
sub77 <sub77@ymail.com>
2015-02-22 21:53:41 +0100

    change to kernel matissewifi
    Change-Id: I9e0797cec201b5723e94f70fb39a2ecd0129b8c0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,11 +22,13 @@ LOCAL_PATH := device/samsung/matissewifi
 TARGET_OTA_ASSERT_DEVICE := matissewifi,matisselte,matisse3g
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/samsung/matissewifi-2
-#TARGET_KERNEL_CONFIG := msm8226-sec_defconfig
-TARGET_KERNEL_CONFIG := msm8226-seckexec_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/matissewifi
+#TARGET_KERNEL_SOURCE := kernel/samsung/matissewifi-2
+TARGET_KERNEL_CONFIG := msm8226-sec_defconfig
+#TARGET_KERNEL_CONFIG := msm8226-seckexec_defconfig
 #TARGET_KERNEL_CONFIG := msm8226-sectwrp_defconfig
 TARGET_KERNEL_VARIANT_CONFIG := msm8226-sec_matissewifi_defconfig
+
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x1e00000
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 
