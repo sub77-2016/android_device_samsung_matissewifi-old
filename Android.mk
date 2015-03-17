f4dabccfc9369713da39e859cc3099e78ed2ac0b
sub77 <sub77@ymail.com>
2015-03-17 05:28:09 +0100

    matissewifi: add keymaster firmware links
    Change-Id: Iaf45dd5c3765fad12815c31ca2c55a6b45b2f37d

--- Android.mk
+++ Android.mk
@@ -18,4 +18,16 @@ ifeq ($(TARGET_DEVICE),matissewifi)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+KEYMASTER_IMAGES := \
+    keymaste.b00 keymaste.b01 keymaste.b02 keymaste.b03 keymaster.mdt
+
+KEYMASTER_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/keymaster/,$(notdir $(KEYMASTER_IMAGES)))
+$(KEYMASTER_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Keymaster firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/`echo $(notdir $@) | sed 's/r\././'` $@
+
+ALL_DEFAULT_INSTALLED_MODULES = $(KEYMASTER_SYMLINKS)
+
 endif
