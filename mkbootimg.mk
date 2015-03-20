7690307466c1f501afdfb8b443411ba555bf2f99
sub77 <sub77@ymail.com>
2015-03-20 20:52:05 +0100

    matissewifi: recovery

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,5 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
+LZMA_BIN := /usr/bin/lzma
+
+CL_GRN="\033[32m"
+PRT_INS := $(CL_GRN)
+PRT_IMG := $(CL_GRN)
+CL_RST="\033[0m"
+
 ## Don't change anything under here. The variables are named MSM8226_whatever
 ## on purpose, to avoid conflicts with similarly named variables at other
 ## parts of the build environment
@@ -43,10 +50,22 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTAL
 	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
 
 ## Overload recoveryimg generation: Same as the original, + --dt arg
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) \
+$(INSTALLED_RECOVERYIMAGEBKP_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) \
 		$(recovery_ramdisk) \
 		$(recovery_kernel)
 	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
+
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) \
+		$(recovery_uncompressed_ramdisk) \
+		$(recovery_ramdisk) \
+		$(recovery_kernel)
+		$(LZMA_BIN) $(recovery_uncompressed_ramdisk)
+		$(hide) cp $(recovery_uncompressed_ramdisk).lzma $(recovery_ramdisk)
+	@echo -e ${CL_CYN}"----- Making compressed recovery image ------"${CL_RST}
+	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+	@echo -e ${CL_CYN}"Made compressed recovery image: $@"${CL_RST}
+
