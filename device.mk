d028bdfe1c4481a88fbaca3968e7d51378632a44
sub77 <sub77@ymail.com>
2015-03-21 22:45:17 +0100

    audio configs

--- device.mk
+++ device.mk
@@ -18,19 +18,18 @@ LOCAL_PATH := device/samsung/matissewifi
 # call the proprietary setup
 $(call inherit-product, vendor/samsung/matissewifi/matissewifi-vendor.mk)
 
-
-
-
-
-
-
-
-
 ## We are a tablet, not a phone
 PRODUCT_CHARACTERISTICS := tablet
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Audio configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
